b8ef811928068bd9ede908cb0abc194f05b92c12
Yura-va@tut.by <Yura-va@tut.by>
2025-08-03 15:05:17 +0300

    WalletOperations added

---
+++ Wallet/WalletOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet;
+
+internal class WalletOperation
+{
+    public string ShowItemsWithAmountAndPrices(Wallet wallet)
+    {
+        var sbResult = new StringBuilder();
+
+        foreach (var item in wallet.Coins)
+        { 
+            sbResult.AppendLine(item.Key + ", amount : " + item.Value.ToString() + wallet.Price);
+        }
+    }
+}
