b4152f9457a5b4c076a873f8c50fffd4267e676e
Yura-va@tut.by <Yura-va@tut.by>
2025-08-04 00:00:00 +0300

    CoingeckoClient added

---
+++ Wallet/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet;
+
+internal class Coin
+{
+    public string Name { get; set; }
+
+    public decimal Price
+    {
+        get; set;
+    }
+
+    public Coin(string name, decimal currentPrice)
+    {
+        
+    }
+
+    private async Task <decimal> GetCurrentPriceAsync(string name)
+    { 
+    
+    }
+}
---
+++ Wallet/CoingeckoClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet;
+
+internal class CoingeckoClient
+{
+    private string  _urlPrefix = "https://api.coingecko.com/api/v3/simple/price?ids=solana,kardiachain,algorand,wax,kira-network,rmrk,launchpool,boson-protocol,moonriver&vs_currencies=usd";
+
+    private string  _urlPostfix = "&vs_currencies=usd";
+}
+
