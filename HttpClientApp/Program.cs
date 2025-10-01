25ec12debb44b760dc6e7b202b1fec2537bfe17c
Yura-va@tut.by <Yura-va@tut.by>
2025-10-01 17:48:54 +0300

    1

--- HttpClientApp/Program.cs
+++ HttpClientApp/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace HttpClientApp
 {
+    
     internal class Program
     {
-        static void Main(string[] args)
+        static HttpClient httpClient = new();
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var content = new StringContent("What is HTTP?");
+
+            using HttpRequestMessage request = new();
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new Uri("https://www.google.com");
+            request.Content = content;
+
+            using HttpResponseMessage response = await httpClient.SendAsync(request);
+            Console.WriteLine($"Status: {response.StatusCode}\n");
+            Console.WriteLine($"Version: {response.Version.ToString()}\n");
+            Console.WriteLine();
+
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+
         }
     }
 }
